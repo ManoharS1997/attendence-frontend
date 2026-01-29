4d2e2b97c44368c97f16666620a38cc135ce0fd0
ManoharS1997 <admin@nowitservices.com>
2026-01-29 18:29:50 +0530

    Update done

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 /* =========================
    BASE URL & AXIOS INSTANCE
 ========================= */
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5001/api";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://44.217.109.241/:5000/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
